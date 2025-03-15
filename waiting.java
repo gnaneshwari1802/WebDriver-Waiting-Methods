2c3e7b76029a1f8fa8ab69e5bbdc60013fea9805
Gnaneshwari Mahimaluru <gnaneshwari.m2009@gmail.com>
2025-03-15 11:42:57 +0530

    Update waiting.java

--- waiting.java
+++ waiting.java
@@ -13,5 +13,20 @@ driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 ge().window().maximize() driver.manage().window().maximize(); 
 driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Admin"); 
 driver.close(); 
+  WebDriver driver-new ChromeDriver(); 
+//Fluent wait declaration 
+Wait<WebDriver> mywait=new FluentWait<WebDriver>(driver) 
+.withTimeout(Duration.ofSeconds(30)) 
+.pollingEvery (Duration.ofSeconds (5)) 
+.ignoring (NoSuchElementException.class); 
+I 
+driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"); driver.manage().window().maximize(); 
+WebElement txtUsername = mywait.until(new Function<WebDriver, WebElement>() { 
+public WebElement apply(WebDriver driver) ( 
+return driver.findElement(By.xpath("//input[@placeholder="Username']")); 
+} 
+}); 
+txtUsername.sendKeys("Admin");
+
 }
 }
